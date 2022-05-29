03aa8288392cb8e00a577da1cb9392870cc29ad2
owentr1369 <tamvaa1306@gmail.com>
2022-05-29 20:49:26 +0700

    styling cartitemcard

--- src/components/cart/CartItemCard.vue
+++ src/components/cart/CartItemCard.vue
@@ -24,4 +24,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.cart-item-card {
+  width: 90%;
+  margin: 5%;
+  background-color: white;
+  box-shadow: 0 0 5px gray;
+  border-radius: 5px;
+  padding: 10px;
+  text-align: left;
+  .header {
+    display: flex;
+    justify-content: space-around;
+  }
+}
 </style>
