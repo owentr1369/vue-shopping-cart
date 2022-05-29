1d10b839983320261272645b4a1f7c6f14d23f8b
owentr1369 <tamvaa1306@gmail.com>
2022-05-29 16:46:09 +0700

    styling productdesdrawer

--- src/components/products/ProductDescriptionDrawer.vue
+++ src/components/products/ProductDescriptionDrawer.vue
@@ -2,7 +2,7 @@
   <div>
     <div
       class="drawer-background"
-      :class="{ show: active.product_drawer }"
+      :class="{ show: active }"
       @click="$emit('close-product-drawer')"
     />
     <div class="drawer" :show="{ show: active }">
@@ -35,5 +35,75 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.drawer-background {
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background-color: rgba(124, 124, 124, 0.55);
+  z-index: 100;
+  display: none;
+  transition: display 0.5s;
+
+  &.show {
+    display: block;
+  }
+}
+.drawer {
+  width: 95vw;
+  height: 100vh;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: -105vw;
+  padding: 15px;
+  transition: left 0.5s;
+  z-index: 101;
+  overflow-y: scroll;
+
+  &.show {
+    left: 0;
+  }
+}
+.drawer-close {
+  font-size: 1.5rem;
+  padding: 5px;
+  border-radius: 5px;
+  right: 10px;
+  border: 2px solid gray;
+  color: gray;
+  width: 15px;
+  float: right;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+}
+.product-detail {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  p.description {
+    padding: 20px;
+    line-height: 1.5rem;
+  }
+  .button-container {
+    button {
+      width: 150px;
+      border: none;
+      padding: 10px;
+      border-radius: 5px;
+      margin: 0 5px 50px 5px;
+      cursor: pointer;
+    }
+  }
+}
+@media (min-width: 500px) {
+  .drawer {
+    width: 450px;
+  }
+}
 </style>
