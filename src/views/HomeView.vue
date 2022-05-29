93396f2f9a8146ab845b87cb5b76b18a88d30401
owentr1369 <tamvaa1306@gmail.com>
2022-05-29 16:31:17 +0700

    product des drawer template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="home">
     <div class="product-cards-container">
+      <ProductDescriptionDrawer
+        :product="product"
+        :active="active.product_drawer"
+      />
       <ProductSummaryCard
         v-for="product in items"
         :key="product.id"
         :product="product"
+        v-on:view-product="viewProduct($event)"
       />
     </div>
   </div>
@@ -13,11 +18,25 @@
 <script>
 import items from "../data/data";
 import ProductSummaryCard from "../components/products/ProductSummaryCard.vue";
+import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
+
 export default {
   name: "HomeView",
-  components: { ProductSummaryCard },
+  components: { ProductSummaryCard, ProductDescriptionDrawer },
   data() {
-    return { items: items };
+    return {
+      items: items,
+      product: null,
+      active: {
+        product_drawer: false,
+      },
+    };
+  },
+  methods: {
+    viewProduct(product) {
+      this.product = product;
+      console.log(this.product);
+    },
   },
 };
 </script>
